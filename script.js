54ef44ea310f261659ceda4c827c5b3d3e484f28
vincaf <vincenzocafassog@gmail.com>
2022-06-23 17:15:54 +0200

    added click on the btn that creates the squares

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ function createNewSquare(){
     return currentSquare;
 }
 
-// Ciclo per l'inserimento dei quadrati nella griglia con relativo numero crescente
-for (let i = 1; i <= 100; i++) {
-    const newSquare = createNewSquare();
-    newSquare.innerHTML = i;
-    gridContainer.append(newSquare);    
-}
+// Creo la variabile di collegamento al bottone nell'html
+let buttonStart = document.getElementById('button-start');
+
+buttonStart.addEventListener('click', function(){
+    // Ciclo per l'inserimento dei quadrati nella griglia con relativo numero crescente
+    for (let i = 1; i <= 100; i++) {
+        const newSquare = createNewSquare();
+        newSquare.innerHTML = i;
+        gridContainer.append(newSquare);    
+    }
+})
+
+
+
 
